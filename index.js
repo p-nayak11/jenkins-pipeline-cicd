32f4f22b4035915deed00bfe39c454c7b6c81c62
Prabhanjan Nayak <44911049+p-nayak11@users.noreply.github.com>
2023-07-12 13:05:04 +0000

    Added files (#1)
    * Create index.js
    
    * Create package.json
    
    * Create process.json

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = process.env.PORT || 9000;
+const app = express();
+
+// Hello World for index page
+app.get('/', function (req, res) {
+    return res.send("Hello World from Prabhanjan Nayak!");
+})
+// Start the server
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+  
