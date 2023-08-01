1b68f8e829cb4b0b38e91b2d550f69438e022f21
Prabhanjan Nayak <44911049+p-nayak11@users.noreply.github.com>
2023-08-01 15:33:34 +0000

    Update vm_auto_deployment.sh

--- vm_auto_deployment.sh
+++ vm_auto_deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-az group create --name pnayakAutoRG --location eastus
-az vm create --name pnayakAutoVM --resource-group pnayakAutoRG --image Ubuntu2204 --subnet xxxxxxxxxx  --authentication-type all --admin-username prabhann --admin-password xxxxxx --public-ip-address '' --custom-data cloud-init-jenkins.txt
-az vm boot-diagnostics enable --name pnayakAutoVM --resource-group pnayakAutoRG
+az group create --name BGDeploymentRG --location eastus
+az vm create --name pnayakUtilityVM --resource-group BGDeploymentRG --image Ubuntu2204 --subnet xxxxxx  --authentication-type all --admin-username xxxxxx --admin-password xxxxxx --public-ip-address '' --custom-data cloud-init-jenkins.txt
+az vm boot-diagnostics enable --name pnayakUtilityVM --resource-group BGDeploymentRG
 echo "Please check Jenkins status using the command 'service jenkins status'"
 echo "Please copy the Jenkins Auth Token at 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword'"
 echo "Follow the rest of the READ.me instructions!"
